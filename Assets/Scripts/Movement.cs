15ae7dc66f02419565602f9b02839a82efaba25b
Can Akdag <canakdag06@gmail.com>
2025-11-14 20:46:03 +0300

    created Movement.cs

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private Vector2 initialDirection;
+    private Vector2 nextDirection;
+    private Vector2 direction;
+    private Vector3 startingPos;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float speedMultiplier = 1.0f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startingPos = transform.position;
+    }
+
+    void Start()
+    {
+        Reset();
+    }
+
+    void Update()
+    {
+        if (nextDirection != Vector2.zero)
+        {
+            SetDirection(direction);
+        }
+    }
+
+    public void Reset()
+    {
+        speedMultiplier = 1.0f;
+        direction = initialDirection;
+        nextDirection = Vector2.zero;
+        transform.position = startingPos;
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        this.enabled = true;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+        Vector2 translation = direction * speed * speedMultiplier * Time.fixedDeltaTime;
+
+        rb.MovePosition(position + translation);
+    }
+
+    public void SetDirection(Vector2 dir, bool forced = false)
+    {
+        if (IsTileValid(dir) || forced)
+        {
+            direction = dir;
+            nextDirection = Vector2.zero;
+        }
+        else
+        {
+            nextDirection = dir;
+        }
+    }
+
+    public bool IsTileValid(Vector2 dir)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0.0f, dir, 1.5f, wallLayer);
+        return hit.collider == null;
+    }
+}
