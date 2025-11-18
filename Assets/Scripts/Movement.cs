79991b6b8e3f1fd34edd9d374a345701c9b1de08
Can Akdag <canakdag06@gmail.com>
2025-11-18 23:28:58 +0300

    rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -92,6 +92,9 @@ public class Movement : MonoBehaviour
         {
             direction = dir;
             nextDirection = Vector2.zero;
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
         else
         {
